85f7562829e00aebccb5c98db3e74c3b8bc3f22e
seitamuro <seitamuro0210@gmail.com>
2025-02-16 00:27:59 +0900

    終了処理を追加

--- src/machines/toggle-machines.ts
+++ src/machines/toggle-machines.ts
@@ -7,6 +7,7 @@ export enum ToggleEvent {
 enum ToggleState {
   INACTIVE = "inactive",
   ACTIVE = "active",
+  FINISHED = "finished",
 }
 
 const maxCount = 3;
@@ -17,15 +18,24 @@ export const toggleMachine = createMachine({
   states: {
     [ToggleState.INACTIVE]: {
       on: {
-        click: {
-          target: "active",
-          guard: ({ context }) => context.count < maxCount, // 状態遷移の条件
-        },
+        click: [
+          {
+            target: ToggleState.ACTIVE,
+            guard: ({ context }) => context.count < maxCount, // 状態遷移の条件
+          },
+          {
+            target: ToggleState.FINISHED,
+            guard: ({ context }) => context.count >= maxCount, // 状態遷移の条件
+          },
+        ],
       },
     },
     [ToggleState.ACTIVE]: {
       entry: assign({ count: ({ context }) => context.count + 1 }),
-      after: { 1000: { target: "inactive" } }, // 2秒後にinactiveに遷移
+      after: { 1000: { target: ToggleState.INACTIVE } }, // 2秒後にinactiveに遷移
+    },
+    [ToggleState.FINISHED]: {
+      type: "final",
     },
   },
 });
