e296622f680217582ad326f927900c4c5537522a
seitamuro <seitamuro0210@gmail.com>
2025-02-15 23:39:26 +0900

    toggleボタンをUIとして実装

--- src/machines/toggle-machines.ts
+++ src/machines/toggle-machines.ts
@@ -1,14 +1,31 @@
-import { createMachine } from "xstate";
+import { assign, createMachine } from "xstate";
 
+export enum ToggleEvent {
+  CLICK,
+}
+
+enum ToggleState {
+  INACTIVE = "inactive",
+  ACTIVE = "active",
+}
+
+const maxCount = 3;
 export const toggleMachine = createMachine({
   id: "toggle",
   initial: "inactive",
+  context: { count: 0 },
   states: {
-    inactive: {
-      on: { click: { target: "active" } },
+    [ToggleState.INACTIVE]: {
+      on: {
+        click: {
+          target: "active",
+          guard: ({ context }) => context.count < maxCount, // 状態遷移の条件
+        },
+      },
     },
-    active: {
-      on: { click: { target: "inactive" } },
+    [ToggleState.ACTIVE]: {
+      entry: assign({ count: ({ context }) => context.count + 1 }),
+      after: { 1000: { target: "inactive" } }, // 2秒後にinactiveに遷移
     },
   },
 });
